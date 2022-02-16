111231fef791f100d153909e47d107d574e1fb8a
이윤주 <toyaann@naver.com>
2022-02-16 17:55:16 +0900

    주석 제작 - 데이터 클래스 멤버변수 선언

--- app/src/main/java/com/nepplus/listview_20220216/datas/StudentData.kt
+++ app/src/main/java/com/nepplus/listview_20220216/datas/StudentData.kt
@@ -1,8 +1,10 @@
 package com.nepplus.listview_20220216.datas
 
+// 데이터 클래스의 하위 정보 값 멤버변수 생성 
+
 class StudentData(
-    val name : String,
-    val birthYear : Int,
-    val address : String,
+    val name : String,  // String 타입 이름
+    val birthYear : Int,  // Int 타입의 생년월일
+    val address : String,  // String 타입의 주소
 ) {
 }
