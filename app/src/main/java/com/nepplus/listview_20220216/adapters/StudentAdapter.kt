d107e4291f41ad08e8e490fd084e17e8e208ccb7
이윤주 <toyaann@naver.com>
2022-02-16 18:05:50 +0900

    주석 제작 - 어댑터 패키지, 어댑터 클래스 제작

--- app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20220216/adapters/StudentAdapter.kt
@@ -1,3 +1,8 @@
+// 1. java 폴더 내의 프로젝트패키지에 하위 패키지 추가 - (adapters)
+// 2. adapter class 제작 (adapter 패키지 하위 클래스 / 담을데이터Adapter 형태로)
+// 어댑터(Adapter)란, 데이터와 리스트 뷰 사이에 존재하는 Object 객체, 데이터와 리스트 뷰 사이의 통신을 위한 다리역할
+// 어댑터는 어댑터뷰가 출력할 수 있는 데이터로 만들어 놓는 공간을 의미, 어댑터뷰는 이 데이터를 출력하는 역할을 하게 된다.
+
 package com.nepplus.listview_20220216.adapters
 
 import android.content.Context
